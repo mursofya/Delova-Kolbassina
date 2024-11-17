635cc841fd63436d2ebc69de45285a8090c83fef
mursofya <sofya.murzaeva@etu.univ-cotedazur.fr>
2024-11-17 23:39:55 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let burger = document.getElementById('burger');
-    burger.addEventListener('click', function(){
-        burger.classList.toggle('open')
-    }
-}
