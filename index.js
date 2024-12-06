cc77476ae2358eea5c4eaab475c3feba16cfd2af
mursofya <sofya.murzaeva@etu.univ-cotedazur.fr>
2024-12-06 11:40:56 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger');
-const nav = document.getElementById('nav');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('open');  // Переключаем класс 'open' для открытия и закрытия меню
-});
