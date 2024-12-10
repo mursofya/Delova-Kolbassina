8f282cce40b938d25e431b60d96d7f0c0d82d993
LouisVedovati <louis.vedovati@etu.unice.fr>
2024-12-10 17:45:12 +0100

    apropos.js

--- apropos.js
+++ apropos.js
@@ -4,6 +4,3 @@ const nav = document.getElementById('nav');
 burger.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
-
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
